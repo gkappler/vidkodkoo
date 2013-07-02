253c12e76b5ee423a6ba39131f28f5586a9e9696
Gregor Kappler <g.kappler@gmx.net>
2013-07-02 16:00:25 +0200

    option whether to run compression finally

--- src/R/recode-config.R
+++ src/R/recode-config.R
@@ -1,20 +1,24 @@
 
 forceOverwrite <- FALSE
-  cleanUp  <- FALSE  ## NOTE: cleanup is broken after change to archiving process, kept for reference
-  compress <- TRUE
+compress <- TRUE
+run.compression <- FALSE
 
-  regexp.restrict <- ".*"  
-  regexp.prio <- ".*tagesmutter.videos.*(jowö|lebl|tiva|bjlö|alwi|emke|luüb|mave|roro|sopr|lucl|lebl|bilö|alwi|luüb|emke|jowö|tiva|mave|sopr|lucl).*"
-#  regexp.Teil <- "_Teil ?[1-9]"
-  regexp.Teil <- "_Teil[1-9]"
-  regexp.videos <- "(MPG|mpg|MOD|mod|mts|MTS)$"
+regexp.restrict <- ".*"  
+regexp.prio <- ".*tagesmutter.videos.*(jowö|lebl|tiva|bjlö|alwi|emke|luüb|mave|roro|sopr|lucl|lebl|bilö|alwi|luüb|emke|jowö|tiva|mave|sopr|lucl).*"
+                                        #  regexp.Teil <- "_Teil ?[1-9]"
+regexp.teil <- "_Teil[1-9]"
+regexp.videos <- "(MPG|mpg|MOD|mod|mts|MTS)$"
 
-  dir.input <- "/data/Videos/unkomprimiert"
-  dir.output <- "/data/Videos/komprimiert"
-  ext.output <- "xvid.avi"
+dir.input <- "/data/Videos/unkomprimiert"
+dir.output <- "/data/Videos/komprimiert"
+ext.output <- "xvid.avi"
 
-  dir.tmp <- "/data/Videos/tmp/"
-  command.file <- "~/recode_videos.sh"  
+dir.tmp <- "/data/Videos/tmp/"
+command.file <- "~/recode_videos.sh"  
 
 
-  dir.archive <- "/data/Videos/Archiv/"
+dir.archive <- "/data/Videos/Archiv/"
+
+
+cleanUp  <- FALSE  ## NOTE: cleanup is broken after change to
+                   ## archiving process, kept for reference
