bc7e5598e0904b8955ce9bb333e3685875b266ef
Gregor Kappler <g.kappler@gmx.net>
2013-07-04 11:38:48 +0200

    new nas share

--- src/R/recode-config.R
+++ src/R/recode-config.R
@@ -5,19 +5,19 @@ run.compression <- FALSE
 
 regexp.restrict <- ".*"  
 regexp.prio <- ".*tagesmutter.videos.*(jowö|lebl|tiva|bjlö|alwi|emke|luüb|mave|roro|sopr|lucl|lebl|bilö|alwi|luüb|emke|jowö|tiva|mave|sopr|lucl).*"
-                                        #  regexp.Teil <- "_Teil ?[1-9]"
+##  regexp.teil <- "_Teil ?[1-9]"
 regexp.teil <- "_Teil[1-9]"
 regexp.videos <- "(MPG|mpg|MOD|mod|mts|MTS)$"
 
-dir.input <- "/data/Videos/unkomprimiert"
-dir.output <- "/data/Videos/komprimiert"
+dir.input <- "/dataNAS/Videos INBOX"
+dir.output <- "/dataNAS/Videos KOMPRIMIERT"
 ext.output <- "xvid.avi"
 
 dir.tmp <- "/data/Videos/tmp/"
 command.file <- "~/recode_videos.sh"  
 
 
-dir.archive <- "/data/Videos/Archiv/"
+dir.archive <- "/dataNAS/Videos ARCHIV/"
 
 
 cleanUp  <- FALSE  ## NOTE: cleanup is broken after change to
