34dec2a971b029a690d0e3b8b25e034971e2b2a6
Gregor Kappler <g.kappler@gmx.net>
2013-07-11 15:24:39 +0200

    do not change owner/group of schnitt definition files

--- src/cut.sh
+++ src/cut.sh
@@ -4,6 +4,7 @@ cd $HOME
 autodir="/data/Videos/komprimiert/"
 
 processLine(){
+## echo $1
     r="video=([a-zA-ZüöäßÖÄÜ0-9._]*) dipl=([a-zA-ZüöäßÖÄÜ]*) ticket=#?([0-9]*) cut.start=(-?[0-9]{2}:-?[0-9]{2}:-?[0-9]{2}) cut.dur=(-?[0-9]{2}:-?[0-9]{2}:-?[0-9]{2})"
     l=`echo "$1" | awk '{print tolower($0)}'`;
 #    echo $l
@@ -66,8 +67,8 @@ done
 find $autodir -name "autoschnitt.*.txt" | while read f
 do
     ## allow editing of txt file for all users in samba group (accessing windows share)
-    chgrp samba "$f"
-    chmod g+w "$f"
+    ## chgrp samba "$f"
+    ## chmod g+w "$f"
 
     schnitttype=`echo $f | sed 's/.*autoschnitt\.//g'`;
     schnitttype=`echo $schnitttype | sed 's/\.txt//g'`;
@@ -85,7 +86,9 @@ do
     lno=1
     iconv -f iso8859-1 -t utf8 $f | while read line
     do
-      echo "\n\n\nprocessing file '$f'"
+      echo
+      echo
+      echo "processing file '$f'"
         if [[ ! "$line" =~ ^[[:space:]]*$ ]]; then      
             echo "       processing '$line'"
             processLine "$line" "$fdir" $schnitttype
