a238495c759bf96bb5d23103f67a53ebf5be2863
Bernhard Piskernik <Bernhard.Piskernik@univie.ac.at>
2013-08-07 15:07:44 +0200

    adjusted to praktifix

--- src/R/merge-act-config.R
+++ src/R/merge-act-config.R
@@ -1,8 +1,10 @@
 
 ## Bitte ausfüllen mit Strings
-  basedir <- Verzeichnis des lokalen git Repositories für trac Daten
-  pubdir  <- "<user>@<host>:<directory>"
-  blacklist.file="blacklist-tapro2012.txt"
+
+  basedir <- "/home/piskerb7/tapro2012"
+  pubdir  <- "/dataNAS/OUTBOX/act/"
+  blacklist.file="/dataNAS/INBOX/blacklist-tapro2012.txt"
   excluded.log <- file("meldung-ausgeschlossene-kodierungen-tapro2012.txt","w",encoding="iso8859-1")
   warning.file <- "meldung-warnung-kodierungen-tapro2012.csv"
   error.log <- "meldung-fehler-kodierungen-tapro2012.txt"
+
