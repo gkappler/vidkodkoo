67fc7c1edb66f890d1c73c8045df4657f793e770
Gregor Kappler <g.kappler@gmx.net>
2013-08-13 11:37:36 +0200

    paths wout " ", avi added

--- src/R/recode-config.R
+++ src/R/recode-config.R
@@ -7,17 +7,17 @@ regexp.restrict <- ".*"
 regexp.prio <- ".*tagesmutter.videos.*(jowö|lebl|tiva|bjlö|alwi|emke|luüb|mave|roro|sopr|lucl|lebl|bilö|alwi|luüb|emke|jowö|tiva|mave|sopr|lucl).*"
 ##  regexp.teil <- "_Teil ?[1-9]"
 regexp.teil <- "_Teil[1-9]"
-regexp.videos <- "(MPG|mpg|MOD|mod|mts|MTS)$"
+regexp.videos <- "(MPG|mpg|MOD|mod|mts|MTS|avi|AVI)$"
 
-dir.input <- "/dataNAS/Videos INBOX"
-dir.output <- "/dataNAS/Videos KOMPRIMIERT"
+dir.input <- "/dataNAS/Videos_INBOX"
+dir.output <- "/dataNAS/Videos_KOMPRIMIERT"
 ext.output <- "xvid.avi"
 
 dir.tmp <- "/data/Videos/tmp/"
 command.file <- "~/recode_videos.sh"  
 
 
-dir.archive <- "/dataNAS/Videos ARCHIV/"
+dir.archive <- "/dataNAS/Videos_ARCHIV/"
 
 
 cleanUp  <- FALSE  ## NOTE: cleanup is broken after change to
